3af2d5f2fb62344f1512eabfa84be7d031ce23c4
Jonathan Tuzman <tuzmusic@gmail.com>
2018-12-18 15:12:22 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
   def create
     @student = Student.new
     @student.first_name = params[:first_name]
-    @student.first_name = params[:last_name]
+    @student.kast_name = params[:last_name]
     @student.save
     binding.pry
     redirect_to student_path(@student)
